6506ab763ed6e8f84f52f612713788437c56184b
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-09 09:45:20 +0300

    feat: add register endpoint to controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common"
 import { AuthService } from "./auth.service"
+import { AuthDto } from "./auth.dto"
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(ValidationPipe)
+  @Post("register")
+  async regsiter(@Body() dto: AuthDto) {
+    return this.authService.register(dto)
+  }
 }
