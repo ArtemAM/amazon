1747833b5e980c113f97831836f49ee95a85b618
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-09 09:46:31 +0300

    feat: add register method to AuthService

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from "@nestjs/common"
+import { BadRequestException, Injectable } from "@nestjs/common"
+import { PrismaService } from "src/prisma.service"
+import { AuthDto } from "./auth.dto"
+import { faker } from "@faker-js/faker"
+import { hash } from "argon2"
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async register(dto: AuthDto) {
+    const existUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    })
+    if (existUser) {
+      throw new BadRequestException("User with this email already exists")
+    }
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.name.fullName(),
+        phone: faker.phone.number({ style: "national" }),
+        avatarPath: faker.image.avatar(),
+        password: await hash(dto.password)
+      }
+    })
+    return user
+  }
+}
