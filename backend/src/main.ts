b19a10250402ec406c4506806b9fc250ba065a5f
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-09 13:37:22 +0300

    chore: set global API prefix and enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from "./app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix("api")
+  app.enableCors()
   await app.listen(process.env.PORT ?? 4000)
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
