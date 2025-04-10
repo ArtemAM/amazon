b21c1b13879aaccde48ab904be5944acc2430f65
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 09:18:41 +0300

    f

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, NotFoundException } from "@nestjs/common"
+import { PrismaService } from "src/prisma.service"
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUserById(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        avatarPath: true,
+        phone: true,
+        password: false,
+        favorites: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            images: true,
+            slug: true
+          }
+        }
+      }
+    })
+
+    if (!user) throw new NotFoundException("User not found")
+    return user
+  }
+}
