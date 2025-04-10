6694cf3458779a82acb19587358d3184707d42a7
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 15:47:04 +0300

    feat: add getUserOverview method

--- backend/src/statistics/statistics.service.ts
+++ backend/src/statistics/statistics.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from "@nestjs/common"
+import { UserService } from "src/user/user.service"
 
 @Injectable()
-export class StatisticsService {}
+export class StatisticsService {
+  constructor(private userService: UserService) {}
+
+  async getUserOverview(userId: number) {
+    const user = await this.userService.getUserById(userId, {
+      orders: {
+        select: {
+          items: true
+        }
+      },
+      reviews: true
+    })
+
+    return [
+      {
+        name: "Orders",
+        value: user.orders.length
+      },
+      {
+        name: "Reviews",
+        value: user.reviews.length
+      },
+      {
+        name: "Favorites",
+        value: user.favorites.length
+      }
+    ]
+  }
+}
