8f84ccae842431a44d68a0d109c26bd09eed9b83
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 13:21:20 +0300

    feat: add update endpoint

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Put,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common"
 import { CategoryService } from "./category.service"
+import { CategoryDto } from "./category.dto"
+import { Auth } from "src/auth/decorators/auth.decorator"
 
 @Controller("categories")
 export class CategoryController {
@@ -8,5 +18,15 @@ export class CategoryController {
   @Get()
   async getAll() {
     return this.categoryService.getAll()
+  }
+
+  @UsePipes(ValidationPipe)
+  @Auth()
+  @Put(":categoryId")
+  async update(
+    @Param("categoryId") categoryId: string,
+    @Body() dto: CategoryDto
+  ) {
+    return this.categoryService.update(+categoryId, dto)
   }
 }
