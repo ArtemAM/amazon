66a33330bea681ae5e5aa52e53d6460094d312c0
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 14:58:56 +0300

    feat: add leaveReview method

--- backend/src/review/review.controller.ts
+++ backend/src/review/review.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common"
 import { ReviewService } from "./review.service"
+import { Auth } from "src/auth/decorators/auth.decorator"
+import { CurrentUser } from "src/auth/decorators/user.decorator"
+import { ReviewDto } from "./review.dto"
 
 @Controller("reviews")
 export class ReviewController {
@@ -8,5 +19,16 @@ export class ReviewController {
   @Get()
   async getAll() {
     return this.reviewService.getAll()
+  }
+
+  @UsePipes(ValidationPipe)
+  @Auth()
+  @Post("leave/:productId")
+  async leaveReview(
+    @CurrentUser("id") userId: number,
+    @Body() dto: ReviewDto,
+    @Param("productId") productId: string
+  ) {
+    return this.reviewService.create(userId, dto, +productId)
   }
 }
