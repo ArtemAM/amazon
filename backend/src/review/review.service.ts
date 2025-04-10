ce40c39c3e446dc5dfff5903141f8ad3a5da4106
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 14:43:57 +0300

    feat: add getAll method

--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common"
+import { PrismaService } from "src/prisma.service"
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll() {
+    return await this.prisma.review.findMany({
+      orderBy: {
+        createdAt: "desc"
+      },
+      select: {
+        id: true,
+        rating: true,
+        text: true,
+        createdAt: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatarPath: true
+          }
+        }
+      }
+    })
+  }
+}
