161231df896d11e328d2db2710a76604b2b730b6
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 09:20:57 +0300

    feat: add getProfile endpoint

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from "@nestjs/common"
+import { Controller, Get } from "@nestjs/common"
 import { UserService } from "./user.service"
+import { CurrentUser } from "src/auth/decorators/user.decorator"
+import { Auth } from "src/auth/decorators/auth.decorator"
 
-@Controller("user")
+@Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get("profile")
+  @Auth()
+  async getProfile(@CurrentUser("id") userId: number) {
+    return this.userService.getUserById(userId)
+  }
 }
