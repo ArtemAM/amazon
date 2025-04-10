cc03daf5796dd02c7947a2ab2093796511d5ad82
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 10:12:35 +0300

    feat: add updateProfile endpoint

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Get,
+  Put,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common"
 import { UserService } from "./user.service"
 import { CurrentUser } from "src/auth/decorators/user.decorator"
 import { Auth } from "src/auth/decorators/auth.decorator"
+import { UserDto } from "./user.dto"
 
 @Controller("users")
 export class UserController {
@@ -11,5 +19,12 @@ export class UserController {
   @Auth()
   async getProfile(@CurrentUser("id") userId: number) {
     return this.userService.getUserById(userId)
+  }
+
+  @UsePipes(ValidationPipe)
+  @Put("profile")
+  @Auth()
+  async updateProfile(@CurrentUser("id") userId: number, @Body() dto: UserDto) {
+    return this.userService.updateProfile(userId, dto)
   }
 }
