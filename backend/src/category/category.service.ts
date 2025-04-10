a13fa30be64319d410597815930baf171e570e1e
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-10 12:51:05 +0300

    feat: add getCategoryById method

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, NotFoundException } from "@nestjs/common"
+import { PrismaService } from "src/prisma.service"
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async getCategoryById(id: number) {
+    const category = await this.prisma.category.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        slug: true
+      }
+    })
+
+    if (!category) throw new NotFoundException("Category not found")
+
+    return category
+  }
+}
