f714231f8be921f8935ae1ca3126d742fcadc5ef
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-11 11:17:05 +0300

    feat: include CategoryService in ReviewModule provider

--- backend/src/review/review.module.ts
+++ backend/src/review/review.module.ts
@@ -4,9 +4,16 @@ import { ReviewController } from "./review.controller"
 import { PrismaService } from "src/prisma.service"
 import { ProductService } from "src/product/product.service"
 import { PaginationService } from "src/pagination/pagination.service"
+import { CategoryService } from "src/category/category.service"
 
 @Module({
   controllers: [ReviewController],
-  providers: [ReviewService, PrismaService, ProductService, PaginationService]
+  providers: [
+    ReviewService,
+    PrismaService,
+    ProductService,
+    PaginationService,
+    CategoryService
+  ]
 })
 export class ReviewModule {}
