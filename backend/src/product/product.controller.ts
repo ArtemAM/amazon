587ba7491a97e895113d5080ba1d24ef50a32673
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-11 09:45:56 +0300

    feat: add getSimilar endpoint

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from "@nestjs/common"
+import { Controller, Get, Param } from "@nestjs/common"
 import { ProductService } from "./product.service"
 
 @Controller("product")
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get("similar/:id")
+  async getSimilar(@Param("id") id: string) {
+    return this.productService.getSimilar(+id)
+  }
 }
