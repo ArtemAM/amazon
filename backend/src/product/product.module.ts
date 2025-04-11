10a3f399d34e4c1df15021b9207fad5671830e5c
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-11 11:17:25 +0300

    feat: include CategoryService in ProductModule provider

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -3,10 +3,16 @@ import { ProductService } from "./product.service"
 import { ProductController } from "./product.controller"
 import { PrismaService } from "src/prisma.service"
 import { PaginationService } from "src/pagination/pagination.service"
+import { CategoryService } from "src/category/category.service"
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService, PrismaService, PaginationService],
+  providers: [
+    ProductService,
+    PrismaService,
+    PaginationService,
+    CategoryService
+  ],
   exports: [ProductService]
 })
 export class ProductModule {}
