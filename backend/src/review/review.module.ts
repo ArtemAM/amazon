62cc68813864b77fdf079bb4dbb5213b4189ae2a
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-11 10:37:07 +0300

    feat: include ProductService and PaginationService in ReviewModule providers

--- backend/src/review/review.module.ts
+++ backend/src/review/review.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common"
 import { ReviewService } from "./review.service"
 import { ReviewController } from "./review.controller"
 import { PrismaService } from "src/prisma.service"
+import { ProductService } from "src/product/product.service"
+import { PaginationService } from "src/pagination/pagination.service"
 
 @Module({
   controllers: [ReviewController],
-  providers: [ReviewService, PrismaService]
+  providers: [ReviewService, PrismaService, ProductService, PaginationService]
 })
 export class ReviewModule {}
