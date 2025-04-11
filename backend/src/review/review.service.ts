24bf04d267e0edba967b16e2586201d976fd6f72
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-11 10:39:03 +0300

    refactor: replace Prisma query with ProductService call in create method

--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -2,10 +2,14 @@ import { Injectable, NotFoundException } from "@nestjs/common"
 import { PrismaService } from "src/prisma.service"
 import { ReviewDto } from "./review.dto"
 import { reviewSelectObject } from "./review.select"
+import { ProductService } from "src/product/product.service"
 
 @Injectable()
 export class ReviewService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private productService: ProductService
+  ) {}
 
   async getAll() {
     return await this.prisma.review.findMany({
@@ -17,9 +21,7 @@ export class ReviewService {
   }
 
   async create(userId: number, dto: ReviewDto, productId: number) {
-    const isExistProduct = await this.prisma.product.findUnique({
-      where: { id: productId }
-    })
+    const isExistProduct = await this.productService.getById(productId)
 
     if (!isExistProduct)
       throw new NotFoundException("User or product not found")
