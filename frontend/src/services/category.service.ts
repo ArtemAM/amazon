6aa2b02d3195e33b46fda29be25986b655f2843b
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-22 11:43:02 +0300

    feat: add getById method

--- frontend/src/services/category.service.ts
+++ frontend/src/services/category.service.ts
@@ -6,5 +6,12 @@ export const CategoryService = {
   async getAll(): Promise<ICategory[]> {
     const response = await instance.get<ICategory[]>(CATEGORY_ENDPOINTS.GET_ALL)
     return response.data
+  },
+
+  async getById(categoryId: string): Promise<ICategory> {
+    const response = await instance.get<ICategory>(
+      CATEGORY_ENDPOINTS.GET_BY_ID.replace(':categoryId', categoryId)
+    )
+    return response.data
   }
 }
