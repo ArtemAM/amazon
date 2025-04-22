8ebaba8ad732f31bf51b2303ade624c8cd0c0ca0
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-22 11:32:35 +0300

    feat: add CATEGORY_ENDPOINTS

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -3,3 +3,12 @@ export const AUTH_ENDPOINTS = {
   REGISTER: '/auth/register',
   REFRESH_TOKEN: '/auth/login/refresh-token'
 }
+
+export const CATEGORY_ENDPOINTS = {
+  GET_ALL: '/categories',
+  GET_BY_ID: '/categories/:categoryId',
+  GET_BY_SLUG: '/categories/by-slug/:slug',
+  UPDATE_BY_ID: '/categories/:categoryId',
+  CREATE: '/categories',
+  DELETE_BY_ID: '/categories/:categoryId'
+}
