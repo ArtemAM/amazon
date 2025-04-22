89668ab2a4a0411fc5d6c9f533e6642a4b9f2d12
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-22 09:38:29 +0300

    feat: add request interceptor

--- frontend/src/api/api.interceptor.ts
+++ frontend/src/api/api.interceptor.ts
@@ -1,7 +1,16 @@
 import { getContentType } from './api.helper'
+import { getAccessTokenFromStorage } from '@/services/auth/auth.helper'
 import axios from 'axios'
 
 const instance = axios.create({
   baseURL: process.env.SERVER_URL,
   headers: getContentType()
+})
+
+instance.interceptors.request.use(config => {
+  const accessToken = getAccessTokenFromStorage()
+  if (config.headers && accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`
+  }
+  return config
 })
