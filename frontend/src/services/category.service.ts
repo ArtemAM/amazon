2b9a7c3c0942f11d8d3f6d353ac9f71d54b3f06d
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-22 11:34:21 +0300

    feat: add CategoryService with getAll method

---
+++ frontend/src/services/category.service.ts
@@ -0,0 +1,10 @@
+import { instance } from '@/api/api.interceptor'
+import { CATEGORY_ENDPOINTS } from '@/config/api'
+import { ICategory } from '@/interfaces/category.interface'
+
+export const CategoryService = {
+  async getAll(): Promise<ICategory[]> {
+    const response = await instance.get<ICategory[]>(CATEGORY_ENDPOINTS.GET_ALL)
+    return response.data
+  }
+}
