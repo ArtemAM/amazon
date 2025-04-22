aae83e14813b8dc4f79dbfb8edc1281c48be71a8
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-22 13:49:50 +0300

    feat: add leaveByProductId method

--- frontend/src/services/review.service.ts
+++ frontend/src/services/review.service.ts
@@ -1,11 +1,26 @@
 import { instance } from '@/api/api.interceptor'
 import { REVIEW_ENDPOINTS } from '@/config/api'
-import { IReviewWithUser } from '@/interfaces/review.interface'
+import {
+  IReviewData,
+  IReviewLeaveResponse,
+  IReviewWithUser
+} from '@/interfaces/review.interface'
 
 export const ReviewService = {
   async getAll(): Promise<IReviewWithUser[]> {
     const response = await instance.get<IReviewWithUser[]>(
       REVIEW_ENDPOINTS.GET_ALL
+    )
+    return response.data
+  },
+
+  async leaveByProductId(
+    productId: string,
+    data: IReviewData
+  ): Promise<IReviewLeaveResponse> {
+    const response = await instance.post(
+      REVIEW_ENDPOINTS.LEAVE.replace(':productId', productId),
+      data
     )
     return response.data
   }
