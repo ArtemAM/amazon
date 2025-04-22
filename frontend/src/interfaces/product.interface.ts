c30ccb3c65915352467a1f040d0c7ea0df6a9932
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-22 17:24:59 +0300

    feat: add IProductBase interface

--- frontend/src/interfaces/product.interface.ts
+++ frontend/src/interfaces/product.interface.ts
@@ -1,19 +1,22 @@
 import { ICategory } from './category.interface'
 import { IReviewWithUser } from './review.interface'
 
-export interface IProductImage {
-  id: number
-  imageUrl: string
-  productId: number
-}
-
-export interface IProduct {
+export interface IProductBase {
   description: string
   id: number
   name: string
   price: number
   createdAt: Date
   slug: string
+}
+
+export interface IProductImage {
+  id: number
+  imageUrl: string
+  productId: number
+}
+
+export interface IProduct extends IProductBase {
   images: IProductImage[]
   reviews: IReviewWithUser[]
   category: ICategory
