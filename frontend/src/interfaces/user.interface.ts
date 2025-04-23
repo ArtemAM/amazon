2e1f9691fa1572c38b2ce644e38f0e26d9e15320
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-23 10:17:12 +0300

    feat: add IUserStatistics type

--- frontend/src/interfaces/user.interface.ts
+++ frontend/src/interfaces/user.interface.ts
@@ -1,14 +1,11 @@
+import { IStatisticItem } from './statistic.interface'
+
 export interface IUser {
   id: number
   email: string
   name: string
   phone: string
   avatarPath: string | null
-}
-
-export interface IStatisticItem {
-  name: 'Orders' | 'Reviews' | 'Favorites'
-  value: number
 }
 
 export type IUserStatistics = IStatisticItem[]
