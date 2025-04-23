924f0bde05264a0acdb386588dfc6af69d8cc3ed
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-23 10:52:33 +0300

    feat: add updateProfile method

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -1,12 +1,21 @@
 import { instance } from '@/api/api.interceptor'
 import { USER_ENDPOINTS } from '@/config/api'
-import { IUserProfile } from '@/interfaces/user.interface'
+import {
+  IUser,
+  IUserProfile,
+  IUserUpdateProfile
+} from '@/interfaces/user.interface'
 
 export const UserService = {
   async getProfile(): Promise<IUserProfile> {
     const response = await instance.get<IUserProfile>(
       USER_ENDPOINTS.GET_PROFILE
     )
+    return response.data
+  },
+
+  async updateProfile(data: IUserUpdateProfile): Promise<IUser> {
+    const response = await instance.put(USER_ENDPOINTS.UPDATE_PROFILE, data)
     return response.data
   },
 
